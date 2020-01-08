96e4f564fb1bf3e1b66eacb778e0ce7db0825a6c
mahdi hellal <mahdihellal@gmail.com>
2020-01-08 01:52:48 +0100

    fix back

--- server/controllers/products_controller.js
+++ server/controllers/products_controller.js
@@ -1,35 +1,33 @@
 const mongoose = require('mongoose');
-const Product  = require('../models/product');
+const Product = require('../models/product');
 module.exports = {
-    readAllProducts(req, res) {
-        //When using mongoose can use a callback or a use a exec method to catch and respond to errors.
-        Product.find({}).exec((err, products) => {
-            if(err) console.log('Get Product Mongoose Error------------------', err);
-            console.log('products-------------', products);
-            res.status(200).send(products);
-        });
-    },
-    readProduct(req, res) {
-        //Destruct the id from the endpoint url, to retrieve  a specific product.
-        const { id } = req.params;
-        //Copy and paste on of the product's id to the url when testing it.
-        //Use the findById to get a specific product.
-        Product.findById(id).exec((err, product) => {
-            if(err) console.log('Get Single Product Error---------------', err);
-            console.log('product--------------', product);
-            res.status(200).json({product});
-        })
-    },
-    readFromCategorie(req, res) {
-        //When using mongoose can use a callback or a use a exec method to catch and respond to errors.
-        const { categorie } = req.params;
+  readAllProducts(req, res) {
+    //When using mongoose can use a callback or a use a exec method to catch and respond to errors.
+    Product.find({}).exec((err, products) => {
+      if (err) console.log('Get Product Mongoose Error------------------', err);
+      console.log('products-------------', products);
+      res.status(200).send(products);
+    });
+  },
+  readProduct(req, res) {
+    //Destruct the id from the endpoint url, to retrieve  a specific product.
+    const { id } = req.params;
+    //Copy and paste on of the product's id to the url when testing it.
+    //Use the findById to get a specific product.
+    Product.findById(id).exec((err, product) => {
+      if (err) console.log('Get Single Product Error---------------', err);
+      console.log('product--------------', product);
+      res.status(200).json({ product });
+    });
+  },
+  readFromCategorie(req, res) {
+    //When using mongoose can use a callback or a use a exec method to catch and respond to errors.
+    const { categorie } = req.params;
 
-        Product.find({categorie:categorie}).exec((err, products) => {
-            if(err) console.log('Get Product Mongoose Error------------------', err);
-            console.log('products-------------', products);
-            res.status(200).send(products);
-        });
-    },
-}
-
-  
+    Product.find({ categorie: categorie }).exec((err, products) => {
+      if (err) console.log('Get Product Mongoose Error------------------', err);
+      console.log('products-------------', products);
+      res.status(200).send(products);
+    });
+  },
+};
